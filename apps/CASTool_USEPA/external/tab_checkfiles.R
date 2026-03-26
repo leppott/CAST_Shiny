fd46870d316bf98dfeabdd8cc0d4bce7845ddf76
Laura Naslund <Naslund.Laura@epa.gov>
2026-03-26 09:53:14 -0400

    Move explore WS stressor to run report, active language and modal text updates, directions to proceed to subsequent tabs, additional modals, match WOE table formatting with report, limit stressor summaries to retained stressors and index/SSI responses

--- apps/CASTool_USEPA/external/tab_checkfiles.R
+++ apps/CASTool_USEPA/external/tab_checkfiles.R
@@ -43,16 +43,32 @@ function() {
 					# Define Files----
 					h2("Identify files"),
 					div(style = "margin-bottom: 20px", 
-						 p(em(paste0("Names for each data input file are specified in the metadata file '",
+						 p(em(paste0("Specific names for each data input file in the metadata file '",
 								fn_default_check_input_cast_metadata,
 								".'")))),
 					# table for imported files
 					# h4("Matching files"),
 					# DT::dataTableOutput("df_import_files_DT"),
-					h4("Missing files"),
+					h4(tagList("Missing files",
+								  icon("info-circle", 
+								  	  style = "color: #67c1f5", 
+								  	  id="missingFilesInfo") |>
+								  	bs_embed_popover(
+								  		title = "Helpful Hints",
+								  		content = "Box will be empty if no missing files are found. If a file is listed as missing, ensure that the file name listed in the metadata is correct and corresponds to a file included in the uploaded zipped folder.",
+								  		placement = "right",
+								  		trigger = "hover"))),
 					p(em("Files included in the metadata but not present in the uploaded zipped folder.")),
 					div(style = "margin-bottom: 20px", pre(textOutput("txt_import_files_missing"))),
-					h4("Extra files"),
+					h4(tagList("Extra files",
+								  icon("info-circle", 
+								  	  style = "color: #67c1f5", 
+								  	  id="extraFilesInfo") |>
+								  	bs_embed_popover(
+								  		title = "Helpful Hints",
+								  		content = "Box will be empty if no extra files are found. If a file is listed as extra, ensure that it does not correspond to a required file input and that all file names are correctly listed in the metadata.",
+								  		placement = "right",
+								  		trigger = "hover"))),
 					p(em("Files in the uploaded zipped folder but not included in the metadata.")),
 					div(style = "margin-bottom: 20px", pre(textOutput("txt_import_files_extra"))),
 					  
@@ -105,7 +121,7 @@ function() {
 						fluidRow(
 							column(6,
 									 p(tagList(
-									 	strong("Exclude outliers: "),
+									 	strong("Exclude outliers user selection: "),
 									 	icon("info-circle", 
 									 		  style = "color: #67c1f5", 
 									 		  id="outlierInfo") |>
@@ -130,38 +146,14 @@ function() {
 								 # 				 selected = "Yes"
 								 # )
 								 ),
-					fluidRow(
-						column(6,
-								 
-								 p(tagList(
-								 	strong("Explore watershed stressor data: "),
-								 	icon("info-circle", 
-								 		  style = "color: #67c1f5", 
-								 		  id="wsStressorInfo") |>
-								 		bs_embed_popover(title = "Helpful Hints",
-								 							  content = "To modify, change the exploreWSStressor parameter in '_CASTool_Metadata.xlsx'.",
-								 							  placement = "right",
-								 							  trigger = "hover"))),
-								 # bsPopover(id="wsStressorInfo", 
-								 # 			 title = HTML("<b>Helpful Hints</b>"), 
-								 # 			 content = HTML("To modify, change the exploreWSStressor parameter in _CASTool_Metadata.xlsx."),
-								 # 			 placement = "right", 
-								 # 			 trigger = "hover"),
-								 
-								 
-								 div(class = "pill", textOutput("txt_setup_explore"))),
-						
-					),
-					#),## fluidRow
-					
-					# 
+
 					h2("Check files"),
 					shinyjs::disabled(bsButton("but_check_check",
 														"Check input files")),
 					bsTooltip(id = "but_check_check",
 								 title = paste0("Enabled after files uploaded"),
 								 placement = "right"),
-					div(style = "width: 50%", p(em("Generate tables checking that input files contain expected columns with expected datatypes and evaluating match ups between paired input data files."))),
+					div(style = "width: 50%", p(em("Generate and review check file tables to ensure that input files contain expected columns with expected datatypes and paired input data files contain expected matchups."))),
 		
 					#h4("Input File Check"),
 					#
@@ -171,7 +163,16 @@ function() {
 					h4("Relational integrity"),
 					DT::dataTableOutput("df_check_qctable2_DT"),
 					
-					h4("Download file check tables"),
+					h4(tagList(
+						"Download file check tables",
+						icon("info-circle", 
+							  style = "color: #67c1f5", 
+							  id="checkFileTabInfo") |>
+							bs_embed_popover(title = "Helpful Hints",
+												  content = "Downloading the file check tables is not required, but allows users to reference identified issues in the input data after the app has timed out.",
+												  placement = "right",
+												  trigger = "hover")
+						)),
 					# p("Download file check tables"),
 					shinyjs::disabled(shiny::downloadButton(
 						"but_check_dload_qctables",
@@ -180,9 +181,16 @@ function() {
 								 title = paste0("Only enabled after files checked."),
 								 placement = "right"),
 					div(style = "width: 50%", 
-							 p(em("Download a zipped folder with the Summary of file inputs and Relational integrity table."))),
+							 p(em("(Optional) Download a zipped folder with the Summary of file inputs and Relational integrity table."))),
 					
-					h2("Download checked data"),
+					h2(tagList("Download checked data",
+								  icon("info-circle", 
+								  	  style = "color: #67c1f5", 
+								  	  id="dwnldCheckInfo") |>
+								  	bs_embed_popover(title = "Helpful Hints",
+								  						  content = "The zipped folder downloaded here is required for the Set Up Tool tab. Users should not need to view files in the checked inputs folder and may skip the Upload and Check Data tab for subsequent CASTool runs by uploading a previously saved checked inputs folder if the input data have not changed.",
+								  						  placement = "right",
+								  						  trigger = "hover"))),
 					div(shinyjs::disabled(shiny::downloadButton(
 						"but_check_dload_rds",
 						"Download checked data"))),
@@ -190,7 +198,9 @@ function() {
 								 title = paste0("Only enabled after files checked"),
 								 placement = "right"),
 					div(style = "width: 50%", 
-							 p(em("Download a zipped folder with all checked data files. This folder is required to run the next step of the CASTool.")))
+							 p(em("Download a zipped folder with all checked data files. This folder is required to run the next step of the CASTool."))),
+					
+					div(em(textOutput("but_check_msg")), style = "color: #62c342")
 
 							)## mainPanel
 				)## tabPanel
--- apps/CASTool_USEPA/external/tab_report.R
+++ apps/CASTool_USEPA/external/tab_report.R
@@ -13,18 +13,30 @@ function() {
 				mainPanel(
 					# doesn't work unless in include this line (even though it is in UI.R)
 					useShinyjs(), 
+					
 					fluidRow(
 						column(4,
 								 p(strong("Target site:")),
 								 div(class = "pill", textOutput("txt_rep_siteid_report")),
 								 br(),
-								 p(strong("Report delivery format:")),
+								 p(strong("Report delivery format user selection:")),
 								 div(class = "pill", textOutput("txt_setup_format")),
 								 # selectInput("report_format",
 								 # 				"Report delivery format",
 								 # 				choices = c("HTML", 
 								 # 								"PDF"),
 								 # 				selected = "HTML"),
+								 br(),
+								 p(tagList(
+								 	strong("Explore watershed stressor data user selection: "),
+								 	icon("info-circle", 
+								 		  style = "color: #67c1f5", 
+								 		  id="wsStressorInfo") |>
+								 		bs_embed_popover(title = "Helpful Hints",
+								 							  content = "To modify, change the exploreWSStressor parameter in '_CASTool_Metadata.xlsx'.",
+								 							  placement = "right",
+								 							  trigger = "hover"))),
+								 div(class = "pill", textOutput("txt_setup_explore")),
 								 br(),
 								 shinyjs::disabled(bsButton("but_report_run",
 								 									"Generate report")),
@@ -33,6 +45,8 @@ function() {
 								 						 placement = "right"),
 								 # p(em("Report should take approximately 5 minutes to generate for every biological community analyzed. A popup window will appear when the report is completed.")),
 								 htmlOutput("txt_rep_time_est"),
+								 br(),
+								 p(em("Runtimes depend on the size of the input data, number of biological communities, number of stressors selected, number of responses selected, and whether the watershed stressor analyses are requested. CASTool results are generated faster using the R console program than the Shiny application.")),
 								 br(),
 								 hr()
 								 )
@@ -44,6 +58,8 @@ function() {
 					shinyBS::bsTooltip(id = "rad_report_tabs",
 											 title = paste0("Disabled until report is created."),
 											 placement = "right"),
+					
+					div(em(textOutput("report_tab_msg")), style = "color: #62c342"),
 					
 					br(),
 					br(),
